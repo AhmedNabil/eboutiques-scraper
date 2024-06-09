5cf36db574caa4b5d9de86b859bfad40fc3baf8f
ahmednabilgm7 <an@MasrpayMacs-MacBook-Air.local>
2024-06-09 15:31:45 +0300

    =init

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ import handler
 import xlsxwriter
 
 
-
 # # Create an new Excel file and add a worksheet.
-# workbook = xlsxwriter.Workbook('products.xlsx')
-# worksheet = workbook.add_worksheet('Products')
-# worksheet2 = workbook.add_worksheet('Ingredients')
+# workbook = xlsxwriter.Workbook("products.xlsx")
+# worksheet = workbook.add_worksheet("Products")
+# worksheet2 = workbook.add_worksheet("Ingredients")
 
 # # Widen the first column to make the text clearer.
 # worksheet.set_column('A:A', 20)
@@ -40,17 +39,6 @@ import xlsxwriter
 # workbook.close()
 
 
-
-
-
-
-
-
-
-
-
-
-
 isHaveNextPage = True
 page = 1
 id = 0
@@ -59,60 +47,7 @@ FILE_PATH = Path("products.xlsx")
 baseUrl = "https://www.ewg.org/skindeep/browse/category/"
 
 
-if not FILE_PATH.exists():
-    with open(FILE_PATH, "w", newline="") as products_csv:
-        workbook = xlsxwriter.Workbook('products.xlsx')
-        products_worksheet = workbook.add_worksheet('Products')
-        ingredients_worksheet = workbook.add_worksheet('Ingredients')
-        bold = workbook.add_format({'bold': True})
-
-        products_worksheet.set_column('A:A', 20)
-        products_worksheet.set_column('B:B', 40)
-        products_worksheet.set_column('C:C', 40)
-        products_worksheet.set_column('D:D', 40)
-        products_worksheet.set_column('E:E', 20)
-        products_worksheet.set_column('F:F', 20)
-        products_worksheet.set_column('G:G', 20)
-        products_worksheet.set_column('H:H', 100)
-        products_worksheet.set_column('I:I', 100)
-        products_worksheet.set_column('J:J', 100)
-
-        products_worksheet.write('A1', 'ID', bold)
-        products_worksheet.write('B1', 'Name', bold)
-        products_worksheet.write('C1', 'Image', bold)
-        products_worksheet.write('D1', 'Product URL', bold)
-        products_worksheet.write('E1', 'Main Category', bold)
-        products_worksheet.write('F1', 'Parent Category', bold)
-        products_worksheet.write('G1', 'Sub Category', bold)
-        products_worksheet.write('H1', 'Ingredients From Packaging', bold)
-        products_worksheet.write('I1', 'Directions From Packaging', bold)
-        products_worksheet.write('J1', 'Warnings From Packaging', bold)
-
-        ingredients_worksheet.set_column('A:A', 20)
-        ingredients_worksheet.set_column('B:B', 80)
-        ingredients_worksheet.set_column('C:C', 20)
-        ingredients_worksheet.set_column('D:D', 150)
-        ingredients_worksheet.set_column('E:E', 150)
-        
-        ingredients_worksheet.write('A1', 'ID', bold)
-        ingredients_worksheet.write('B1', 'Ingredient Name', bold)
-        ingredients_worksheet.write('C1', 'Product ID', bold)
-        ingredients_worksheet.write('D1', 'FUNCTION(S)', bold)
-        ingredients_worksheet.write('E1', 'CONCERNS', bold)
-
-        workbook.close()
-
-
-
-
-
-
-
-
-
-
-
-
+handler.xlsx_file(FILE_PATH)
 
 
 for idx, category in enumerate(cats):
@@ -146,20 +81,71 @@ for idx, category in enumerate(cats):
                 product_company = handler.product_handler(product)["product_company"]
                 product_img = handler.product_handler(product)["product_img"]
                 product_label_information = product_details["label_information"]
-                ingredient_from_packaging =  product_label_information["ingredients_from_packaging"]
-                directions_from_packaging = product_label_information["directions_from_packaging"] if "directions_from_packaging" in product_label_information  else "N/A"
-                warnings_from_packaging = product_label_information["warnings_from_packaging"] if "warnings_from_packaging" in product_label_information  else "N/A"
-                product_ingredient_concerns = product_details["product_ingredient_concerns"]
-                print ("====================================================================================================")
+                ingredient_from_packaging = product_label_information[
+                    "ingredients_from_packaging"
+                ]
+                directions_from_packaging = (
+                    product_label_information["directions_from_packaging"]
+                    if "directions_from_packaging" in product_label_information
+                    else "N/A"
+                )
+                warnings_from_packaging = (
+                    product_label_information["warnings_from_packaging"]
+                    if "warnings_from_packaging" in product_label_information
+                    else "N/A"
+                )
+                product_ingredient_concerns = product_details[
+                    "product_ingredient_concerns"
+                ]
+                print(
+                    "===================================================================================================="
+                )
                 print(f"ID : {id}")
                 print(f"Product Name : {product_name}")
                 print(f"Product Company : {product_company}")
-                print (f"Product Image : {product_img}")
+                print(f"Product Image : {product_img}")
                 print(f"Ingredients From Packaging : {ingredient_from_packaging}")
                 print(f"Directions From Packaging : {directions_from_packaging}")
                 print(f"Warnings From Packaging : {warnings_from_packaging}")
-                print(f"Product Ingredient Concerns : {product_ingredient_concerns}")
-                print ("====================================================================================================")
+                # print(f"Product Ingredient Concerns : {product_ingredient_concerns}")
+                print(
+                    "===================================================================================================="
+                )
+                handler.write_product_to_xlsx(
+                    FILE_PATH,
+                    {
+                        "id": id,
+                        "product_name": product_name,
+                        "product_img": product_img,
+                        "product_company": product_company,
+                        "product_url": product_url,
+                        "main_category": main_cat,
+                        "parent_category": parent_cat,
+                        "sub_category": sub_cat,
+                        "ingredient_from_packaging": ingredient_from_packaging,
+                        "directions_from_packaging": directions_from_packaging,
+                        "warnings_from_packaging": warnings_from_packaging,
+                        "product_ingredient_concerns": product_ingredient_concerns,
+                    },
+                )
+                for ingredient in product_details["product_ingredient_concerns"]:
+                    handler.write_ingredient_to_xlsx(
+                        FILE_PATH,
+                        {
+                            "ingredient_name": ingredient["ingredient_name"],
+                            "product_id": id,
+                            "ingredient_function": (
+                                ingredient["ingredient_function"]
+                                if "ingredient_function" in ingredient
+                                else "N/A"
+                            ),
+                            "ingredient_concerns": (
+                                ingredient["ingredient_concerns"]
+                                if "ingredient_concerns" in ingredient
+                                else "N/A"
+                            ),
+                        },
+                    )
                 # with open(FILE_PATH, "a", newline="") as products_csv:
                 #     products_csv_write = csv.writer(products_csv)
                 #     products_csv_write.writerow(
@@ -173,7 +159,7 @@ for idx, category in enumerate(cats):
                 #             parent_cat,
                 #             sub_cat,
                 #             ingredient_from_packaging,
-                #             directions_from_packaging, 
+                #             directions_from_packaging,
                 #             warnings_from_packaging,
                 #             product_details["product_ingredient_concerns"],
 
--- handler.py
+++ handler.py
@@ -8,7 +8,11 @@ import requests
 import csv
 from pathlib import Path
 from const import cats
-import handler
+import xlsxwriter
+from openpyxl import load_workbook
+import openpyxl
+import pandas as pd
+import uuid
 
 
 def url_handler(baseUrl, slug, pagination_slug, page):
@@ -91,26 +95,28 @@ def fetch_product_ingredient_concerns(soup):
     )
 
     ingredient_more_info_list = (
-         soup.find("section", class_="product-concerns-and-info")
+        soup.find("section", class_="product-concerns-and-info")
         .find("table", class_="table-ingredient-concerns")
         .find("tbody")
         .find_all("tr", class_="ingredient-more-info-wrapper")
     )
-    
+
     product_ingredient_concern = []
     for idx, ingredient in enumerate(ingredient_list):
         ingredient_concern = {}
-        ingredient_concern["ingredient_name"] = ingredient.find(
-            "td", class_="td-ingredient"
-        ).find('div' , class_="td-ingredient-interior").text.strip()
+        ingredient_concern["ingredient_name"] = (
+            ingredient.find("td", class_="td-ingredient")
+            .find("div", class_="td-ingredient-interior")
+            .text.strip()
+        )
         for heading in ingredient_more_info_list[idx].find_all(["td"]):
             if "CONCERNS" in heading.text:
                 ingredient_concern["ingredient_concerns"] = heading.find_next("td").text
             if "FUNCTION(S)" in heading.text:
                 ingredient_concern["ingredient_function"] = heading.find_next("td").text
         product_ingredient_concern.append(ingredient_concern)
-    return product_ingredient_concern 
-  
+    return product_ingredient_concern
+
 
 def fetch_label_information(soup):
     info_list = []
@@ -148,17 +154,95 @@ def img_download(url, id):
         response = requests.get(url)
         if response.status_code == 403:
             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-            response = requests.get(url, headers=headers  , stream=True)
+            response = requests.get(url, headers=headers, stream=True)
         if not response.ok:
-             print(response)
+            print(response)
         for block in response.iter_content(1024):
             if not block:
                 break
             file.write(block)
-        
 
 
+def xlsx_file(FILE_PATH):
+    if not FILE_PATH.exists():
+        with open(FILE_PATH, "w", newline="") as products_xlsx:
+            workbook = xlsxwriter.Workbook("products.xlsx")
+            products_worksheet = workbook.add_worksheet("Products")
+            ingredients_worksheet = workbook.add_worksheet("Ingredients")
+            bold = workbook.add_format({"bold": True})
+
+            products_worksheet.set_column("A:A", 20)
+            products_worksheet.set_column("B:B", 40)
+            products_worksheet.set_column("C:C", 40)
+            products_worksheet.set_column("D:D", 40)
+            products_worksheet.set_column("E:E", 20)
+            products_worksheet.set_column("F:F", 20)
+            products_worksheet.set_column("G:G", 20)
+            products_worksheet.set_column("H:H", 100)
+            products_worksheet.set_column("I:I", 100)
+            products_worksheet.set_column("J:J", 100)
+
+            products_worksheet.write("A1", "ID", bold)
+            products_worksheet.write("B1", "Name", bold)
+            products_worksheet.write("C1", "Image", bold)
+            products_worksheet.write("D1", "Product URL", bold)
+            products_worksheet.write("E1", "Main Category", bold)
+            products_worksheet.write("F1", "Parent Category", bold)
+            products_worksheet.write("G1", "Sub Category", bold)
+            products_worksheet.write("H1", "Ingredients From Packaging", bold)
+            products_worksheet.write("I1", "Directions From Packaging", bold)
+            products_worksheet.write("J1", "Warnings From Packaging", bold)
+
+            ingredients_worksheet.set_column("A:A", 20)
+            ingredients_worksheet.set_column("B:B", 80)
+            ingredients_worksheet.set_column("C:C", 20)
+            ingredients_worksheet.set_column("D:D", 150)
+            ingredients_worksheet.set_column("E:E", 150)
+
+            ingredients_worksheet.write("A1", "ID", bold)
+            ingredients_worksheet.write("B1", "Ingredient Name", bold)
+            ingredients_worksheet.write("C1", "Product ID", bold)
+            ingredients_worksheet.write("D1", "FUNCTION(S)", bold)
+            ingredients_worksheet.write("E1", "CONCERNS", bold)
+            print("Workbook Created")
+            workbook.close()
 
 
+def get_sheets(FILE_PATH):
+    wb = load_workbook(FILE_PATH)
+    sheets = wb.sheetnames
+    print(wb.worksheets)
+    return sheets
 
 
+def write_product_to_xlsx(FILE_PATH, product):
+    wb = load_workbook(FILE_PATH)
+    ws = wb["Products"]
+    img_download(product["product_img"], product["id"])
+    img = openpyxl.drawing.image.Image(f"imgs/{product['id']}.png")
+    row = ws.max_row + 1
+    img.anchor = f"C{row}"
+    ws.cell(row=row, column=1, value=product["id"])
+    ws.cell(row=row, column=2, value=product["product_name"])
+    ws.cell(row=row, column=3, value=product["product_img"])
+    # ws.add_image(img, f"C{row}")
+    ws.cell(row=row, column=4, value=product["product_url"])
+    ws.cell(row=row, column=5, value=product["main_category"])
+    ws.cell(row=row, column=6, value=product["parent_category"])
+    ws.cell(row=row, column=7, value=product["sub_category"])
+    ws.cell(row=row, column=8, value=product["ingredient_from_packaging"])
+    ws.cell(row=row, column=9, value=product["directions_from_packaging"])
+    ws.cell(row=row, column=10, value=product["warnings_from_packaging"])
+    wb.save(FILE_PATH)
+
+
+def write_ingredient_to_xlsx(FILE_PATH, ingredient):
+    wb = load_workbook(FILE_PATH)
+    ws = wb["Ingredients"]
+    row = ws.max_row + 1
+    ws.cell(row=row, column=1, value=uuid.uuid4().hex)
+    ws.cell(row=row, column=2, value=ingredient["ingredient_name"])
+    ws.cell(row=row, column=3, value=ingredient["product_id"])
+    ws.cell(row=row, column=4, value=ingredient["ingredient_function"])
+    ws.cell(row=row, column=5, value=ingredient["ingredient_concerns"])
+    wb.save(FILE_PATH)
